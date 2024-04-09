3bd2100eec137808978900ff84fa8d1711d3ff52
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2024-04-09 14:09:54 -0400

    Update string autofind

--- src/Main.asm
+++ src/Main.asm
@@ -10,43 +10,49 @@ macro Text(OFFSET, TEXT) {
   map '\n', 0x0A // New line
 
   origin {OFFSET}
-  variable lengthh = 0;
-  while (read(origin() + lengthh) != 0x00) {
+  variable availableLength = 0;
+  while (read(origin() + availableLength) != 0x00) {
     ds 1
   }
 
-  if (read(origin()+lengthh) == 0x00) {
+  if (read(origin()) == 0x00) {
     fill 1
   }
-  if (read(origin()+lengthh) == 0x00) {
+  if (read(origin()) == 0x00) {
     fill 1
   }
-  if (read(origin()+lengthh) == 0x00) {
+  if (read(origin()) == 0x00) {
     fill 1
   }
-  if (read(origin()+lengthh) == 0x00) {
+  if (read(origin()) == 0x00) {
     fill 1
   }
 
-  lengthh = origin() - {OFFSET}
+  availableLength = origin() - {OFFSET} - 1
   
   origin {OFFSET}
   db {TEXT} // ASCII Text To Print
 
-  variable diff = origin() - {OFFSET}
-  if (diff < lengthh) {
+  variable newLength = origin() - {OFFSET}
+  if (newLength > availableLength) {
     print {TEXT}
     print " is too big by "
-    print (lengthh - diff)
+    print (newLength - availableLength)
   }
 
   while (read(origin()) != 0x00) {
     fill 1
-  }
+  } 
 }
 
 // Warning: use address first!!
 macro Text(TEXT) {
+  variable i = 40;
+  while (i > 0 && read(origin()) < 0x01) {
+    ds 1
+    i = i - 1;
+  }
+  
   variable ori = origin()
   Text(ori, {TEXT})
 }
